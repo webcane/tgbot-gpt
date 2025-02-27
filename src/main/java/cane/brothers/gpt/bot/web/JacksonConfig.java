a95911cc772711ce4701bc4a453b5154b7da8263
Mikhail Niedre <mikhail.niedre@quantori.com>
2025-02-27 21:10:22 +0300

    FAIL_ON_EMPTY_BEANS

--- src/main/java/cane/brothers/gpt/bot/web/JacksonConfig.java
+++ src/main/java/cane/brothers/gpt/bot/web/JacksonConfig.java
@@ -1,6 +1,7 @@
 package cane.brothers.gpt.bot.web;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +13,7 @@ class JacksonConfig {
     @Bean
     ObjectMapper objectMapper() {
         return JsonMapper.builder().build()
+                .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
                 .registerModule(new JavaTimeModule());
     }
 }
