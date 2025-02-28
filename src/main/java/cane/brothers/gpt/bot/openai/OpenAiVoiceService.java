1d4fcdd464392957c375d535776a85b68878011d
Mikhail Niedre <mikhail.niedre@quantori.com>
2025-02-28 13:33:36 +0300

    use transcriptionOptions

--- src/main/java/cane/brothers/gpt/bot/openai/OpenAiVoiceService.java
+++ src/main/java/cane/brothers/gpt/bot/openai/OpenAiVoiceService.java
@@ -5,6 +5,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.ai.audio.transcription.AudioTranscriptionPrompt;
 import org.springframework.ai.audio.transcription.AudioTranscriptionResponse;
 import org.springframework.ai.openai.OpenAiAudioTranscriptionModel;
+import org.springframework.ai.openai.OpenAiAudioTranscriptionOptions;
+import org.springframework.ai.openai.api.OpenAiAudioApi;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
 
@@ -16,7 +18,11 @@ public class OpenAiVoiceService {
     private final OpenAiAudioTranscriptionModel voiceModel;
 
     public String transcribe(Resource fileResource) {
-        AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(fileResource);
+        var transcriptionOptions = OpenAiAudioTranscriptionOptions.builder()
+                .responseFormat(OpenAiAudioApi.TranscriptResponseFormat.TEXT)
+                .temperature(0f)
+                .build();
+        AudioTranscriptionPrompt transcriptionRequest = new AudioTranscriptionPrompt(fileResource, transcriptionOptions);
         AudioTranscriptionResponse response = voiceModel.call(transcriptionRequest);
         log.debug(response.getResult().toString());
         // todo handle errors
