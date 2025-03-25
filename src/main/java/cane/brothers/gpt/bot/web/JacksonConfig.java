f74b1bd37534734ecbbab70c13b276e7e6b0185c
Mikhail Niedre <mikhail.niedre@quantori.com>
2025-03-25 15:26:22 +0300

    fix openai unrecognized annotations

--- src/main/java/cane/brothers/gpt/bot/web/JacksonConfig.java
+++ src/main/java/cane/brothers/gpt/bot/web/JacksonConfig.java
@@ -2,6 +2,7 @@ package cane.brothers.gpt.bot.web;
 
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.json.JsonMapper;
@@ -20,6 +21,11 @@ class JacksonConfig {
     ObjectMapper objectMapper() {
         var objMapper = JsonMapper.builder().build()
                 .configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false)
+                // to avoid UnrecognizedPropertyException: Unrecognized field "annotations"
+                // (class org.springframework.ai.openai.api.OpenAiApi$ChatCompletionMessage)
+                // see https://github.com/spring-projects/spring-ai/pull/2460
+                // see https://github.com/spring-projects/spring-ai/issues/2449
+                .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
                 .registerModule(new JavaTimeModule());
         objMapper.configOverride(ByteArrayOutputStream.class)
                 .setVisibility(JsonAutoDetect.Value.construct(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY));
